f7980ecea48474cfef13af353cabff6257a85329
Andrew Ivan Soegeng <andrew.ivan@ymail.com>
2021-04-14 23:54:08 +0800

    Revert "Revert "initial commit""
    This reverts commit 14bf53af7e21465e1545abf7ada43ce7500cca08.

---
+++ stream.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+from urllib.request import urlopen
+import os
+import datetime
+import time
+import sys
+
+url="http://192.168.1.1:80"
+CAMERA_BUFFER_SIZE=4096
+stream=urlopen(url + "/stream.jpg")
+bts=b''
+i=100
+mtx = np.load('mtx.npy')
+newcameramtx = np.load('newcameramtx.npy')
+dist = np.load('dist.npy')
+roi = np.load('roi.npy')
+
+while True:
+    try:
+        bts+=stream.read(CAMERA_BUFFER_SIZE)
+        jpghead=bts.find(b'\xff\xd8')
+        jpgend=bts.find(b'\xff\xd9')
+        if jpghead>-1 and jpgend>-1:
+            jpg=bts[jpghead:jpgend+2]
+            bts=bts[jpgend+2:]
+            img=cv2.imdecode(np.frombuffer(jpg,dtype=np.uint8),cv2.IMREAD_UNCHANGED)
+            #img=cv2.resize(img,(640,480))
+            img = cv2.undistort(img, mtx, dist, None, newcameramtx)
+            x, y, w, h = roi
+            img = img[y:y + h, x:x + w]
+            cv2.imshow("ESP32 CAM OPENCV stream",img)
+        k=cv2.waitKey(1)
+    except Exception as e:
+        print("Error:" + str(e))
+        bts=b''
+        stream=urlopen(url)
+        continue
+    # Press 'a' to take a picture
+    if k & 0xFF == ord('a'):
+        cv2.imwrite(str(i) + ".jpg", img)
+        print(f"Save image filename: {i}.jpg")
+        i=i+1
+    # Press 'q' to quit
+    if k & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
